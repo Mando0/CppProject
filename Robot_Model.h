d8e5526b9656e80f5bcb03eb00032869041f2204
Juni0 <junidev0@gmail.com>
2016-10-06 06:33:13 -0500

    Shop and Robot Model Class

---
+++ Robot_Model.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Robot_Model {
+  private:
+    string name;
+    int model_number;
+    double price;
+    double max_speed;
+    //vector<Robot_Part> robot_parts;
+  public:
+    Robot_Model(string rm_name, int rm_model_number, double rm_price, double rm_max_speed) :
+      name(rm_name), model_number(rm_model_number), price(rm_price), max_speed(rm_max_speed) {}
+    double get_max_speed() {
+      return max_speed;
+    }
+    string get_name() {
+      return name;
+    }
+    double get_price() {
+      return price;
+    }
+    int get_model_number() {
+      return model_number;
+    }
+};
---
+++ shop.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Robot_Model.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class Shop {
+  public:
+    void create_model() {
+      Robot_Model robot("robot", 1, 10.00, 100.00);
+      robot_model.push_back(robot);
+    }
+    void create_component();
+    string get_robot_name() {
+      return robot_model[0].get_name();
+    }
+    int get_robot_model_number() {
+      return robot_model[0].get_model_number();
+    }
+    int get_robot_price() {
+      return robot_model[0].get_price();
+    }
+    double get_robot_max_speed() {
+      return robot_model[0].get_max_speed();
+    }
+  private:
+    vector<Robot_Model> robot_model;
+};
